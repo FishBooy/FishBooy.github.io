9ba95976ed5605f7c6a1e5b2cc9dd072aded7d01
Calvin <qwk.love@gmail.com>
2020-04-03 02:12:07 +0800

    modified:   themes/tidyNote/source/js/script.js

--- themes/tidyNote/source/js/script.js
+++ themes/tidyNote/source/js/script.js
@@ -1,46 +1,49 @@
-;(function(){
-
+(function() {
   /* Add className for prism parser */
-  const codeBlocks = document.querySelectorAll('code');
-  const supportedLang = ['javascript','css','html','jsx','sass','scss']
-  codeBlocks.forEach(function(code){
+  const codeBlocks = document.querySelectorAll("code");
+  const supportedLang = ["javascript", "css", "html", "jsx", "sass", "scss"];
+  codeBlocks.forEach(function(code) {
     const classList = code.classList;
     const className = classList.value.toLowerCase();
-    if (className && classList.length === 1 && supportedLang.indexOf(className) !== -1) {
+    if (
+      className &&
+      classList.length === 1 &&
+      supportedLang.indexOf(className) !== -1
+    ) {
       classList.add(`language-${className}`);
       classList.add(`lang-${className}`);
 
-      const prev = code.closest('pre');
-      prev && prev.classList.add('line-numbers');
+      const prev = code.closest("pre");
+      prev && prev.classList.add("line-numbers");
     } else if (!className) {
-      classList.add('inline');
+      classList.add("inline");
     }
   });
 
   /* Catalog function */
   // List the content
-  const postBody = document.querySelector('.post-body');
+  const postBody = document.querySelector(".post-body");
   const childNodesInPost = postBody ? postBody.childNodes : [];
-  const htmlTagsForList = ['h2','h3','h4','h5','h6'];
+  const htmlTagsForList = ["h2", "h3", "h4", "h5", "h6"];
   const contentList = [];
 
-  childNodesInPost.forEach((child)=>{
-    const tagName=child.tagName;
-    if(tagName && htmlTagsForList.indexOf(tagName.toLowerCase())!= -1){
-        contentList.push(child);
+  childNodesInPost.forEach(child => {
+    const tagName = child.tagName;
+    if (tagName && htmlTagsForList.indexOf(tagName.toLowerCase()) != -1) {
+      contentList.push(child);
     }
   });
 
   if (contentList.length) {
-    const postCatalog = document.querySelector('.post-catalog');
-    const catalogContainer = document.createElement('ul');
-    postCatalog.classList.add('show');
-    catalogContainer.className = 'catalog-container';
+    const postCatalog = document.querySelector(".post-catalog");
+    const catalogContainer = document.createElement("ul");
+    postCatalog.classList.add("show");
+    catalogContainer.className = "catalog-container";
     postCatalog.append(catalogContainer);
 
-    contentList.forEach(function(node){
-      const listNode = document.createElement('li');
-      const linkNode = node.querySelector('a').cloneNode(true);
+    contentList.forEach(function(node) {
+      const listNode = document.createElement("li");
+      const linkNode = node.querySelector("a").cloneNode(true);
       listNode.className = `${node.tagName.toLowerCase()}-link`;
       if (linkNode) {
         linkNode.innerText = node.textContent;
@@ -50,12 +53,12 @@
     });
 
     // Change position while scrolling
-    const html = document.querySelector('html');
+    const html = document.querySelector("html");
     const originalTop = parseInt(window.getComputedStyle(postCatalog).top);
-    window.addEventListener('scroll', function(){
+    window.addEventListener("scroll", function() {
       const scrollTop = html.scrollTop;
-      postCatalog.style.top = scrollTop < originalTop ? `${Math.abs(originalTop-scrollTop)}px` : 0;
-    })
+      postCatalog.style.top =
+        scrollTop < originalTop ? `${Math.abs(originalTop - scrollTop)}px` : 0;
+    });
   }
-
 })();
